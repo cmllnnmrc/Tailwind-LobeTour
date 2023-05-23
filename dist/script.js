a4a8ba571d6d90e10953b45772b4d71f7bc14c95
cmllnnmrc <cuetolovely0118@gmail.com>
2023-05-23 15:04:08 +0800

    Help 1st Commit

--- dist/script.js
+++ dist/script.js
@@ -14,3 +14,35 @@ links.forEach((down) => {
     down.querySelector("i").classList.toggle("open");
   });
 });
+
+const faqs = document.querySelectorAll(".faq__ques h4");
+faqs.forEach((drop) => {
+  drop.addEventListener("click", () => {
+    drop.nextElementSibling.classList.toggle("open");
+    drop.querySelector("i").classList.toggle("open");
+  });
+});
+
+const faqNav = document.querySelectorAll(".star-nav-link");
+const faqContent = document.querySelectorAll(".coffee__tab__content");
+faqNav.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    removeActiveStar();
+    nav.classList.add("active");
+    const activeContent = document.querySelector(`#${nav.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  faqNav.forEach((nav) => {
+    nav.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  faqContent.forEach((nav) => {
+    nav.classList.remove("active");
+  });
+}
