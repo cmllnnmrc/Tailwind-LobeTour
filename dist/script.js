b341b29cfe421a490e00aa486fe6045078997291
cmllnnmrc <cuetolovely0118@gmail.com>
2023-05-23 15:31:28 +0800

    Help 2nd Commit

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,11 @@
+const faqs = document.querySelectorAll(".faq__ques h4");
+faqs.forEach((drop) => {
+  drop.addEventListener("click", () => {
+    drop.nextElementSibling.classList.toggle("open");
+    drop.querySelector("i").classList.toggle("open");
+  });
+});
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".header__actions");
 const header = document.querySelector(".header");
@@ -15,34 +23,26 @@ links.forEach((down) => {
   });
 });
 
-const faqs = document.querySelectorAll(".faq__ques h4");
-faqs.forEach((drop) => {
-  drop.addEventListener("click", () => {
-    drop.nextElementSibling.classList.toggle("open");
-    drop.querySelector("i").classList.toggle("open");
+const faqNav = document.querySelectorAll(".faq-nav-link");
+const faqContent = document.querySelectorAll(".faq__tab__content");
+faqNav.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    removeActiveFaq();
+    faq.classList.add("active");
+    const activeFaqContent = document.querySelector(`#${faq.id}-content`);
+    removeActiveFaqContent();
+    activeFaqContent.classList.add("active");
   });
 });
 
-const faqNav = document.querySelectorAll(".star-nav-link");
-const faqContent = document.querySelectorAll(".coffee__tab__content");
-faqNav.forEach((nav) => {
-  nav.addEventListener("click", () => {
-    removeActiveStar();
-    nav.classList.add("active");
-    const activeContent = document.querySelector(`#${nav.id}-content`);
-    removeActiveContent();
-    activeContent.classList.add("active");
-  });
-});
-
-function removeActiveStar() {
-  faqNav.forEach((nav) => {
-    nav.classList.remove("active");
+function removeActiveFaq() {
+  faqNav.forEach((faq) => {
+    faq.classList.remove("active");
   });
 }
 
-function removeActiveContent() {
-  faqContent.forEach((nav) => {
-    nav.classList.remove("active");
+function removeActiveFaqContent() {
+  faqContent.forEach((faq) => {
+    faq.classList.remove("active");
   });
 }
